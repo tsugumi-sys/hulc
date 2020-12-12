645c96cd562a82924a94edc10033c629d5138d4b
tsugumi-sys <tidemark0105@gmail.com>
2020-12-12 15:06:06 +0900

    edit document.js

--- pages/_document.js
+++ pages/_document.js
@@ -8,17 +8,15 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang="en">
+      <Html lang="ja">
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,500;0,600;0,700;1,300&display=swap"
             rel="stylesheet"
           />
-          <meta
-            name="description"
-            content="Landingpage VPN LaslesVPN Best VPN For Privacy, Country and Cheapest"
-          />
+          <meta content="HULC(北大ローコード)のホームページです。北海道大学のサークルとして2020年12月から活動を開始。北大IT系サークルとしてアプリケーション開発を通して他の学生団体（部活・サークル）を支援していくことが目的です。" name="description" />
+          <meta charSet="utf-8" />
         </Head>
         <body>
           <Main />
