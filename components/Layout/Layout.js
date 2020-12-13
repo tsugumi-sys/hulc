a514762c1a9186f7ad832170ab39a01980c7568b
tsugumi-sys <tidemark0105@gmail.com>
2020-12-13 14:53:43 +0900

    twitter card meta

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -7,12 +7,16 @@ const Layout = ({ children }) => {
   return (
     <>
       <Head>
+        {/* Twitter */}
         <meta name="twitter:card" content="summary_large_image" key="twcard" />
-        <meta name="twitter:site" content="@HulcLow" />
-        <meta property="og:url" content="https://hulc-git-main.tsugumi-sys.vercel.app/" key="ogurl" />
-        <meta property="og:title" content="HULC HP" key="ogsitename" />
-        <meta property="og:description" content="HULC(北大ローコード)のホームページです。" key="ogdesc" />
+        <meta name="twitter:creator" content="@HulcLow" key="twhandle" />
+
+        {/* Open Graph */}
+        <meta property="og:url" content="https://hulc.vercel.app/" key="ogurl" />
         <meta property="og:image" content="/assets/twitter-card.png" key="ogimage" />
+        <meta property="og:site_name" content="Hokkaido Univ. Low-Code Club" key="ogsitename" />
+        <meta property="og:title" content="HULC(北大ローコード)" key="ogtitle" />
+        <meta property="og:description" content="HULC(北大ローコード)のホームページです。" key="ogdesc" />
       </Head>
       <Header />
       {children}
