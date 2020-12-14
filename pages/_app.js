b0f79d59ca3718e911698317a98e1c69c6da2c38
tsugumi-sys <tidemark0105@gmail.com>
2020-12-14 16:58:29 +0900

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
 import "../styles/tailwind.css";
 import "../styles/slick.css";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouterChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouterChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouterChange)
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />;
 }
 
--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,24 @@ class MyDocument extends Document {
     return (
       <Html lang="ja">
         <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletamanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`}
+          />
+          <script
+             dangerouslySetInnerHTML={{
+               __html: `
+               window.dataLayer = window.dataLayer || [];
+               function gtag(){dataLayer.push(arguments);}
+               gtag('js', new Date());
+               
+               gtag('config', '${process.env.GA_TRACKING_ID}', {
+                 page_path: window.location.pathname,
+               });
+             `,
+             }}
+          />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,500;0,600;0,700;1,300&display=swap"
