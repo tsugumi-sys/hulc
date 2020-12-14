971740c75a06f6f89913ec2740322216ecb22e1a
tsugumi-sys <tidemark0105@gmail.com>
2020-12-14 17:31:26 +0900

    modify code

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import { GA_TRACKING_ID } from '../lib/gtag'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -13,7 +14,7 @@ class MyDocument extends Document {
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
             async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`}
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
           />
           <script
              dangerouslySetInnerHTML={{
@@ -22,7 +23,7 @@ class MyDocument extends Document {
                function gtag(){dataLayer.push(arguments);}
                gtag('js', new Date());
                
-               gtag('config', '${process.env.GA_TRACKING_ID}', {
+               gtag('config', '${GA_TRACKING_ID}', {
                  page_path: window.location.pathname,
                });
              `,
