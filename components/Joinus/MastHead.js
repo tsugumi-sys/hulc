0d749d3c9bef91f6a2a728a19ed9772b71f2f2d4
tsugumi-sys <tidemark0105@gmail.com>
2020-12-21 20:36:22 +0900

    hello world

--- components/Joinus/MastHead.js
+++ components/Joinus/MastHead.js
@@ -6,15 +6,15 @@ function MastHead() {
     <div className="max-w-screen-xl mt-24 px-8 xl:px-16 mx-auto" id="about">
       <div className="grid grid-flow-row sm:grid-flow-col grid-rows-2 md:grid-rows-1 sm:grid-cols-2 gap-8 py-6 sm:py-16 ">
         <div className=" flex flex-col justify-center items-start row-start-2 sm:row-start-1">
-          <h1 className="text-3xl lg:text-4xl xl:text-5xl font-medium text-black-600 leading-normal">
-            Do you want to be a member of <strong>HULC</strong>?
+          <h1 className="leading-relaxed text-3xl lg:text-4xl xl:text-4xl font-semibold text-black-600">
+             あと２人くらい仲間募集中！！！
           </h1>
           <p className="text-black-500 mt-4 mb-6">
-            プログラミングってなんか難しそう…
+            作ったばかりのサークルで初期メンバーを探しています。
             <br />
-            少し勉強したけどどうしたらいいかわかんない…
+            将来エンジニアやIT系志望の人、ぜひ僕と一緒にやってみませんか？
             <br />
-            No(Low)-Codeはプログラミングの入り口、ステップアップに最適です
+            行動力があるそこのあなた！！DM待ってます。
           </p>
         </div>
         <div className="flex w-full">
