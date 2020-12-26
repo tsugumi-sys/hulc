5de56965bd83d535b2a6a1cdb704e6db5de1e5ae
tsugumi-sys <tidemark0105@gmail.com>
2020-12-26 19:28:44 +0900

    loading

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setTimeout(() => setLoading(false), 3000);
+    setTimeout(() => setLoading(false), 4000);
   }, [])
 
   return (
