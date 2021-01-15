47781054775f7a13b4e41d45efe230a52271cb42
tsugumi-sys <tidemark0105@gmail.com>
2021-01-15 20:58:35 +0900

    unnko

--- components/TechBlog/MorePost.js
+++ components/TechBlog/MorePost.js
@@ -17,14 +17,14 @@ export default function Post({
                 <img alt="Placeholder" className="block h-72 w-full object-cover" src={coverImage} />
                 <header className="flex items-center justify-between leading-tight p-2 md:p-4">
                     <h1 className="text-lg">
-                            {title.length > 25 ? title.substr(0, 24) + "..." : title}
+                            {title.length > 15 ? title.substr(0, 14) + "..." : title}
                     </h1>
                     <p className="text-grey-darker text-sm">
                         
                         {moment(date).format("MM/DD/YYYY")}
                     </p>
                 </header>
-                <p className="w-full px-4 text-left">{subtitle.length > 35 ? subtitle.substr(0, 34) + "..." : subtitle}</p>
+                <p className="w-full px-4 text-left">{subtitle.length > 20 ? subtitle.substr(0, 19) + "..." : subtitle}</p>
 
                 <footer className="flex items-center justify-between leading-none p-2 md:p-4">
                     <img alt="Placeholder" className="block rounded-full w-12 h-12 object-cover" src={authorImage} />
