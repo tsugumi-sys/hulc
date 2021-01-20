78120ca58fefb39a9b154834d5942b9295f140d7
tsugumi-sys <tidemark0105@gmail.com>
2021-01-20 22:10:40 +0900

    ss

--- components/AppList/Post.js
+++ components/AppList/Post.js
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import moment from "moment"
 
 export default function Post({
     title,
@@ -6,6 +7,7 @@ export default function Post({
     authorName,
     authorImage,
     slug,
+    date,
     coverImage,
 }) {
     return (
@@ -14,11 +16,15 @@ export default function Post({
             <article className="overflow-hidden rounded-lg shadow-lg hover:shadow-mui">
                 <img alt="Placeholder" className="block h-48 w-full object-cover" src={coverImage} />
                 <header className="flex items-center justify-between leading-tight p-2 md:p-4">
-                    <h1 className="text-lg">
+                    <h1 className="text-base">
                             {title.length > 15 ? title.substr(0, 14) + "..." : title}
                     </h1>
+                    <p className="text-grey-darker text-sm">
+                        
+                        {moment(date).format("MM/DD/YYYY")}
+                    </p>
                 </header>
-                <p className="w-full px-4 text-left">{subtitle.length > 23 ? subtitle.substr(0, 22) + "..." : subtitle}</p>
+                <p className="w-full px-4 text-left text-sm">{subtitle.length > 23 ? subtitle.substr(0, 22) + "..." : subtitle}</p>
 
                 <footer className="flex items-center justify-between leading-none p-2 md:p-4">
                     <img alt="Placeholder" className="block rounded-full w-12 h-12 object-cover" src={authorImage} />
